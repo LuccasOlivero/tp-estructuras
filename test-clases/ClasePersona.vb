407c1680de06fab771fdc58c3bac57cebf80bcb6
lucas <lucas.chorolqui@gmail.com>
2025-05-01 14:28:29 -0300

    update

---
+++ test-clases/ClasePersona.vb
@@ -0,0 +1,25 @@
+﻿Public Class ClasePersona
+
+    Private cmpApellido As String
+    Private cmpdireccion As String
+    Private cmpCiudad As String
+    Private cmpNombre As String
+
+    Public Property Nombre As String
+        Get
+            Return cmpNombre
+        End Get
+        Set(value As String)
+            cmpNombre = value
+        End Set
+    End Property
+
+    Public Property Apellido As String
+        Get
+            Return cmpApellido
+        End Get
+        Set(value As String)
+            cmpApellido = value
+        End Set
+    End Property
+End Class
--- test-clases/Form1.vb
+++ test-clases/Form1.vb
@@ -14,21 +14,5 @@
         TextBox2.Text = ObjCliente.Deuda
     End Sub
 
-    Public Property Codigo As Integer
-        Get
-            Return cmpCodigo
-        End Get
-        Set(cod As Integer)
-            cmpCodigo = cod
-        End Set
-    End Property
 
-    Public Property Deuda As Decimal
-        Get
-            Return cmpDeuda
-        End Get
-        Set(Deu As Decimal)
-            cmpDeuda = Deu
-        End Set
-    End Property
 End Class
